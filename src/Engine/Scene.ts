075dbb99073222fff01b8d156de1e15d9dfa405d
Chad Casey <gbchadd@gmail.com>
2026-08-02 09:40:00 -0700

    Consolidate lights and move data into data dir

--- src/Engine/Scene.ts
+++ src/Engine/Scene.ts
@@ -1,15 +1,9 @@
 import { SceneData, Primative, Light } from "../Utility/types";
 import { spheres } from "../Primitives/Sphere";
-import { pointLights } from "../Light/PointLight";
-import { directionalLights } from "../Light/DirectionalLight";
-import { ambientLights } from "../Light/AmbientLight";
+import { pointLights, directionalLights, ambientLights } from "../Data/Light";
 
 export const primatives: Primative[] = [...spheres];
-export const lights: Light[] = [
-  ...pointLights,
-  ...directionalLights,
-  ...ambientLights,
-];
+export const lights: Light[] = [...pointLights, ...directionalLights, ...ambientLights];
 
 // scene data
 export const sceneData: SceneData = { lights, primatives };
--- src/Light/AmbientLight.ts
+++
@@ -1,5 +0,0 @@
-import { AmbientLight } from "../Utility/types";
-
-export const ambientLights: AmbientLight[] = [
-  { type: "ambient", intensity: 0.2, color: [255, 255, 255] },
-];
